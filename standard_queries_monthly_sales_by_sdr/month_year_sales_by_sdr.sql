3cf38f26e25700b435bb99f916b5ea808a1e7ed5
rsnyderaustin <rsnyder.austin@gmail.com>
2023-06-28 14:04:57 -0500

    Deleted redundant folder

--- standard_queries_monthly_sales_by_sdr/month_year_sales_by_sdr.sql
+++
@@ -1,4 +0,0 @@
-SELECT strftime('%Y-%m', won_date) as year_month, lead_type, COUNT(strftime('%m-%Y', won_date)) as num_sales
-FROM olist_closed_deals_dataset
-GROUP BY strftime('%m-%Y', won_date), lead_type
-ORDER BY year_month ASC
