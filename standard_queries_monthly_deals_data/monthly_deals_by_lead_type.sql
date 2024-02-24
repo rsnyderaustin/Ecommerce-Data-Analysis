8dfba186178fedf116ee9f5325200046c48d92b1
rsnyderaustin <rsnyder.austin@gmail.com>
2024-02-24 10:47:04 -0600

    Deleted unnecessary files, added database-schema

--- standard_queries_monthly_deals_data/monthly_deals_by_lead_type.sql
+++
@@ -1,4 +0,0 @@
-SELECT strftime('%Y-%m', won_date) as year_month, business_type, COUNT(strftime('%m-%Y', won_date)) as num_sales
-FROM olist_closed_deals_dataset
-GROUP BY strftime('%m-%Y', won_date), business_type
-ORDER BY year_month ASC
